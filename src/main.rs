00dffcc35cca5b2e57533cf6330472c3470dab01
Carlos Valiente <carlos@pepelabs.net>
2021-03-19 20:36:36 +0000

    Logging.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
@@ -5,6 +6,7 @@ use zero2prod::startup::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let config = get_configuration().expect("Cannot read config file");
 
     let db_pool = PgPool::connect(&config.database.connection_string())
